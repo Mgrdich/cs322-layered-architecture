2cb1ab7f68d8d483674d84ca6b1d25d40da77d8c
Mgrdich <mgotm13@gmail.com>
2024-04-07 17:30:33 +0400

    Rename Test Application names

--- webapp-iom/src/test/java/am/cs322/UsersApplicationTests.java
+++ webapp-iom/src/test/java/am/cs322/UsersApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Cs322HomeworkApplicationTests {
+class UsersApplicationTests {
 
     @Test
     void contextLoads() {
